3d76bcfdc7db01c208fcfcfe37edafb93abe5cd6
AndreiButalov <andrej.butalov@gmx.de>
2026-03-24 11:04:58 +0100

    add models->Video

--- videoflix_app/apps.py
+++ videoflix_app/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class VideoflixAppConfig(AppConfig):
     name = 'videoflix_app'
+
+    # def ready(self):
+    #     from . import signals
--- videoflix_app/signals.py
+++ videoflix_app/signals.py
@@ -1,10 +1,12 @@
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from .models import Video
 
+
+@receiver(post_save.connect, sender=Video)
 def create_lecture(sender, instance, created, **kwargs):
     if created:
         print('New object created')
 
 
 
-post_save.connect(create_lecture, sender=Video)
