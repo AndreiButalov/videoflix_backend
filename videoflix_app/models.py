fdd287b428ce869deebcb7b6f8e733a63c475642
AndreiButalov <andrej.butalov@gmx.de>
2026-03-24 10:41:08 +0100

    add models->Video

--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    thumbnail_url = models.ImageField(upload_to='thumbnails/')
+    category = models.CharField(max_length=100)
+
+    # # optional: Pfad zum HLS-Ordner (z.B. /media/hls/movie_1/)
+    # hls_path = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
