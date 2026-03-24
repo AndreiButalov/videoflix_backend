a4cd2acfaceaee21be0dfa6b19743c2930856ae7
AndreiButalov <andrej.butalov@gmx.de>
2026-03-24 10:55:08 +0100

    add models->Video

--- videoflix_app/signals.py
+++ videoflix_app/signals.py
@@ -1,5 +1,10 @@
 from django.db.models.signals import post_save
+from .models import Video
 
 def create_lecture(sender, instance, created, **kwargs):
     if created:
         print('New object created')
+
+
+
+post_save.connect(create_lecture, sender=Video)
