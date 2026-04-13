1c08c18180acd689e5eb3575fa4ec871403ede17
AndreiButalov <andrej.butalov@gmx.de>
2026-04-13 09:13:13 +0200

    video model -> add video_file field

--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -4,11 +4,11 @@ class Video(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    thumbnail_url = models.ImageField(upload_to='thumbnails/')
-    category = models.CharField(max_length=100)
 
-    # # optional: Pfad zum HLS-Ordner (z.B. /media/hls/movie_1/)
-    # hls_path = models.CharField(max_length=500)
+    video_file = models.FileField(upload_to='videos/', null=True, blank=True)
+    thumbnail_url = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
+
+    category = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
