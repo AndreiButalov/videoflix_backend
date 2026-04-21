6e06688180e71b4af4014b5b92fa165476da4036
AndreiButalov <andrej.butalov@gmx.de>
2026-04-21 12:32:23 +0200

    refactor, bugfix RegistrationView,ActivateAccountView

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .serializers import RegistrationSerializer
-from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_decode
 from rest_framework import status
 from django.contrib.auth.models import User
@@ -11,6 +10,8 @@ from django.core.mail import send_mail
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.sites.shortcuts import get_current_site
+import uuid
+from django.core.cache import cache
 
 class RegistrationView(APIView):
     permission_classes = [AllowAny]
@@ -21,8 +22,10 @@ class RegistrationView(APIView):
         if serializer.is_valid():
             user, token = serializer.save()
 
-            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            token = str(uuid.uuid4())
+            cache.set(f"activation_{user.id}", token, timeout=3600)
 
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
             domain = get_current_site(request).domain
 
             activation_link = f"http://{domain}/api/activate/{uid}/{token}/"
@@ -51,15 +54,15 @@ class ActivateAccountView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, uidb64, token):
-        try:
-            uid = urlsafe_base64_decode(uidb64).decode()
-            user = User.objects.get(pk=uid)
-        except:
-            return Response({'error': 'Invalid link'}, status=status.HTTP_400_BAD_REQUEST)
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User.objects.get(pk=uid)
 
-        if default_token_generator.check_token(user, token):
+        saved_token = cache.get(f"activation_{user.id}")
+        
+        if token == saved_token:
             user.is_active = True
             user.save()
-            return Response({'message': 'Account activated'}, status=status.HTTP_200_OK)
 
-        return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "Account activated"}, status=200)
+
+        return Response({"error": "Invalid token"}, status=400)
