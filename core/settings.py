b9370f55e98a5c3cbf8c291597baafe8dc96c846
AndreiButalov <andrej.butalov@gmx.de>
2026-04-22 08:55:40 +0200

    settings.py -> update

--- core/settings.py
+++ core/settings.py
@@ -127,8 +127,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
-EMAIL_HOST_USER="kokos3101@gmail.com"
-EMAIL_HOST_PASSWORD="qcpq nsmj husr mysb"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
 
